80b76b9bcff2b2d0eb4892dbf5122ec8321ee496
bradbusby <brad@hashicorp.com>
2018-01-19 15:52:09 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 variable "name" {
-default = "Brad"
+  default = "Bob"
 }
 
 resource "random_id" "random" {
-byte length = 24
+  byte length = 24
 }
 
 output "hello_world" {
-value = "Hello, ${var.name}, ${var.name}"
+  value = "Hello, ${var.name}, ${var.name}"
 }
